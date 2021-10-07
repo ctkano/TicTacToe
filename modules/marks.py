3efcb2517c36a0da015ae76ba9700c90f0f52aa3
Cleyton Kano <cleyton.kano@thomsonreuters.com>
2021-10-07 18:05:03 -0300

    Add module for marks (X, O)

---
+++ modules/marks.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+""" module: marks """
+
+def machine():
+    """ Represents the mark for the machine
+	"""
+    return 'X'
+
+def human():
+    """ Represents the mark for the human
+	"""
+    return 'O'
