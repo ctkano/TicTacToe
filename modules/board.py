103bc4f1fa8064bb1364196287c7fbfbd04f2b34
Cleyton Kano <cleyton.kano@thomsonreuters.com>
2021-10-07 18:06:38 -0300

    Organize module board

--- modules/board.py
+++ modules/board.py
@@ -2,6 +2,14 @@
 
 """ module: board """
 
+from modules import marks as mark
+
+border = None
+field = None
+central_field_space = None
+markMachine = None
+markHuman = None
+
 def display(board):
 	""" Displays/Prints the current game board.
 
@@ -11,14 +19,29 @@ def display(board):
             The game board.
 	"""
 
-	print("+-------" * 3,"+", sep="")
+	#region Board drawing
+	global border
+	global field
+	global central_field_space
+
+	if border is None:
+		border = (("+" + "-" * 7) * 3) + "+" # define the border
+	
+	if field is None:
+		field = (("|"+" " * 7) * 3) + "|" # define the field, below and above the central field
+
+	if central_field_space is None:
+		central_field_space = " " * 3 # dfine the central field spaces, in the left and right of the field value
+	#endregion
+
+	print(border, sep="")
 	for row in range(3):
-		print("|       " * 3,"|", sep="")
+		print(field, sep="")
 		for col in range(3):
-			print("|   " + str(board[row][col]) + "   ", end="")
+			print("|", central_field_space, str(board[row][col]), central_field_space, sep="", end="")
 		print("|")
-		print("|       " * 3,"|",sep="")
-		print("+-------" * 3,"+",sep="")
+		print(field,sep="")
+		print(border,sep="")
 
 def make_list_of_free_fields(board):
 	""" Lists the free fields in the game board.
@@ -34,10 +57,21 @@ def make_list_of_free_fields(board):
 			The list of free fields in the game board.
 	"""
 
+	#region Marks
+	global markMachine
+	global markHuman
+
+	if markMachine is None:
+		markMachine = mark.machine()
+
+	if markHuman is None:
+		markHuman = mark.human()
+	#endregion
+
 	free = []	# the list is empty initially
 	for row in range(3): # iterate through rows
 		for col in range(3): # iterate through columns
-			if board[row][col] not in ['O','X']: # is the cell free?
+			if board[row][col] not in [markHuman,markMachine]: # is the cell free?
 				free.append((row,col)) # yes, it is - append new tuple to the list
 	return free
 
@@ -46,7 +80,7 @@ def empty():
 		
 		Returns
     	----------
-		game_board : list
+		game_board : list[list[int]]
 			A list that represents the game board.
 	"""
 
