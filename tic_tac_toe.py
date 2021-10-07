23baa34992f44484de6640caa38c98236cff9781
Cleyton Kano <cleyton.kano@thomsonreuters.com>
2021-10-07 18:06:50 -0300

    Organize main module

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,45 +2,113 @@
 
 """ module: main """
 
-from sys import path
-path.append('..\\modules')
+import sys
+sys.path.append('..\\modules')
 
 import modules.board as board
+import modules.marks as mark
 import modules.movement as movement
 import modules.criteria as criteria
+import modules.console as console
 
-game_board = board.empty()
+markMachine = None
+markHuman = None
+game_board = []
 
-human_turn = False
+def load_marks():
+	""" Loads the game marks
+	"""
 
-game_board_tuple = movement.machine(game_board) # get the first machine movement
-game_board[game_board_tuple[0]][game_board_tuple[1]] = 'X'# set the first machine movement
-del game_board_tuple
+	global markMachine
+	global markHuman
 
-free = board.make_list_of_free_fields(game_board)
+	#region Marks
+	if markMachine is None:
+		markMachine = mark.machine()
 
-human_turn = True
+	if markHuman is None:
+		markHuman = mark.human()
+	#endregion
 
-while len(free):
-	board.display(game_board)
-	if human_turn:
-		game_board_tuple = movement.user(game_board)
-		game_board[game_board_tuple[0]][game_board_tuple[1]] = 'O'
-		del game_board_tuple
+def game_board_movement(human_turn):
+	""" Draws the movement according to the turn
 
-		victory = criteria.victory_for(game_board,'O')
-	else: #machine turn
-		game_board_tuple = movement.machine(game_board)
-		game_board[game_board_tuple[0]][game_board_tuple[1]] = 'X'
-		del game_board_tuple
+		Parameters
+        ----------
+        human_turn : bool
+            Indicates the turn, if True then it is the human turn, if False it is the machine turn.
+	"""
 
-		victory = criteria.victory_for(game_board,'X')
-	if victory != None:
-		break
-	human_turn = not human_turn		
+	load_marks()
+
+	game_board_tuple = movement.user(game_board) if human_turn else movement.machine(game_board) # create a tuple to represent the field that will be marked
+
+	game_board[game_board_tuple[0]][game_board_tuple[1]] = markHuman if human_turn else markMachine # add mark in the field
+
+	del game_board_tuple # delete tuple
+
+def game():
+	""" Represents the Tic Tac Toe itself
+	"""
+
+	global game_board
+
+	console.clear()
+
+	game_board = board.empty()
+
+	load_marks()
+
+	human_turn = False # define the first turn as the machine
+
+	game_board_movement(human_turn) # set the first machine movement
+
 	free = board.make_list_of_free_fields(game_board)
 
-board.display(game_board)
+	human_turn = True # define the next turn as the human
 
-print(criteria.check_victory(victory))
+	while len(free):
+		console.clear()
 
+		board.display(game_board)
+
+		game_board_movement(human_turn)
+
+		if human_turn:
+			victory = criteria.victory_for(game_board,markHuman)
+		else: # machine turn
+			victory = criteria.victory_for(game_board,markMachine)
+		if victory != None:
+			break
+
+		human_turn = not human_turn	
+
+		free = board.make_list_of_free_fields(game_board)
+
+	console.clear()
+
+	board.display(game_board)
+
+	print(criteria.check_victory(victory))
+
+def start():
+	""" Starts the game
+	"""
+
+	try:
+		game() # call the game
+		
+		reset = input("Do you want to play again? Type Y for Yes or N for No and the game will end: ")
+
+		if reset.upper() == "Y" or reset.upper() == "YES":
+			start() # restart the game
+		else:
+			sys.exit() # exit system
+	except KeyboardInterrupt:
+		print("\nToo bad you don't want to keep playing...\n")
+		sys.exit() # exit system
+
+"""
+	Starts the game!!!
+"""
+start()
